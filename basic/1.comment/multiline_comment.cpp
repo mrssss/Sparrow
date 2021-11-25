2a4688c2958a5131c408ed872355ea41650a08e6
Qijun Niu <qijun.niu@timeplus.io>
2021-11-25 16:23:20 +0000

    add quzz about comments

---
+++ basic/1.comment/multiline_comment.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void multiline_comment()
+{
+    // inline comment can alse used to comment multiline,
+    // comment four lines so that this function will only print one line
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+}
