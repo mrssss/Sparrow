e5740e3a885def0a6e05786332dd6c9c4ff11571
mrssss <niuqijun0702@qq.com>
2021-12-10 22:32:06 +0800

    update the hints of asm add (#11)

--- basic/2.asm/add.s
+++ basic/2.asm/add.s
@@ -3,7 +3,7 @@ add:
     pushq %rbp
     movq %rsp, %rbp
 
-    # a: -8(%rbp), b: -16(%rbp)
+    # a: %rdi, b: %rsi
     # return value should be set to %rax
     # TODO: calculate the value of %rdi + %rsi, and set it to %rax
 
