f484004e9972a75cf9a31f97beccf82e52931ea4
Qijun Niu <qijun.niu@timeplus.io>
2021-12-23 13:23:32 +0000

    add quzz about implement bool operation with bit_set and bit_clear

---
+++ basic/3.number/bool_op.cpp
@@ -0,0 +1,28 @@
+#include <cstdint>
+
+extern uint64_t bit_set(uint64_t x, uint64_t m);
+extern uint64_t bit_clear(uint64_t x, uint64_t m);
+
+uint64_t bool_or(uint64_t x, uint64_t y)
+{
+    uint64_t result = 0;
+    /// TODO: implement the 'or' logic. You can only use bit_set and bit_clear method. `|` is forbidden.
+
+    return result;
+}
+
+uint64_t bool_and(uint64_t x, uint64_t y)
+{
+    uint64_t result = 0;
+    /// TODO: implement the 'and' logic. You can only use bit_set and bit_clear method. `&` is forbidden.
+
+    return result;
+}
+
+uint64_t bool_xor(uint64_t x, uint64_t y)
+{
+    uint64_t result = 0;
+    /// TODO: implement the 'xor' logic. You can only use bit_set and bit_clear method. `^` is forbidden.
+
+    return result;
+}
